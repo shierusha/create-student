9646ea8155b1909056cf35e6415179432100cfdf
Roro <tinacat1518@gmail.com>
2025-07-25 03:06:30 +0900

    Update main.js

--- stidcard/sql-id/main.js
+++ stidcard/sql-id/main.js
@@ -16,8 +16,8 @@
 // (2) Supabase 初始化
 // ==========================
 // 建立 supabase client（如果你的 JS 裡已經有就不用重複寫）
-  const client = window.supabase
-    ? window.supabase.createClient(
+window.client = window.supabase
+  ? window.supabase.createClient(
         'https://wfhwhvodgikpducrhgda.supabase.co',
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmaHdodm9kZ2lrcGR1Y3JoZ2RhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMTAwNjEsImV4cCI6MjA2MzU4NjA2MX0.P6P-x4SxjiR4VdWH6VFgY_ktgMac_OzuI4Bl7HWskz8'
       )
